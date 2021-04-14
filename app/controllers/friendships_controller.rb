883cfb9062ab671eae32cab7db2608651b97b2fe
unknown <paulclue20@gmail.com>
2021-04-14 15:13:34 -0500

    check second row friend acceptance

--- app/controllers/friendships_controller.rb
+++ app/controllers/friendships_controller.rb
@@ -1,7 +1,7 @@
 class FriendshipsController < ApplicationController
   def create
     @friendship = Friendship.create!(friend_id: params[:friend_id], user_id: current_user.id, confirmed: false)
-
+   
     redirect_to users_path if @friendship.save
   end
 
@@ -11,6 +11,7 @@ class FriendshipsController < ApplicationController
     @friendship_two = Friendship.new(user_id: @friendship.friend_id, friend_id: @friendship.user_id,
                                      confirmed: true)
     @friendship_two.save
+    
     flash[:notice] = 'Friendship has been accepted'
     redirect_to users_path
   end
