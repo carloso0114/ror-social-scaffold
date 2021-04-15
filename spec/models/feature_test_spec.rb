d472f48aa094000d5f512f02f889d13be0dec84a
Carlos Ospina <carlossospinag@gmail.com>
2021-04-15 17:26:43 -0500

    update mutual friendship rspec test

--- spec/models/feature_test_spec.rb
+++ spec/models/feature_test_spec.rb
@@ -131,30 +131,32 @@ RSpec.describe 'Likes test', type: :feature do
   end
 end
 
-# rubocop:disable Metrics/BlockLength
-
-RSpec.describe 'Confirm Friendship request', type: :feature do
-  it 'accept friendship to check inverse row in friendship table' do
-    visit new_user_registration_path
-    within('.new_user') do
-      fill_in 'Name', with: 'carlos'
-      fill_in 'Email', with: 'carlos@gmail.com'
-      fill_in 'Password', with: '123456'
-      fill_in 'Password confirmation', with: '123456'
-    end
-    click_button 'Sign up'
-    visit root_path
+RSpec.describe 'Friendship test 2', type: :feature do
+  let :out do
     within('.nav') do
       click_link 'Sign out'
     end
+  end
+  def new_user(name, email, password)
     visit new_user_registration_path
     within('.new_user') do
-      fill_in 'Name', with: 'carlos2'
-      fill_in 'Email', with: 'carlos2@gmail.com'
-      fill_in 'Password', with: '123456'
-      fill_in 'Password confirmation', with: '123456'
+      fill_in 'Name', with: name
+      fill_in 'Email', with: email
+      fill_in 'Password', with: password
+      fill_in 'Password confirmation', with: password
     end
     click_button 'Sign up'
+  end
+  let :accept_friendship do
+    within('.users-list') do
+      click_link 'Accept Friendship'
+    end
+  end
+  it 'checks mutual friendships' do
+    new_user('carlos', 'carlos@gmail.com', '123456')
+    visit root_path
+    out
+    new_user('carlos2', 'carlos2@gmail.com', '123456')
     visit users_path
     within('.users-list') do
       click_link 'Add Friend'
@@ -175,8 +177,9 @@ RSpec.describe 'Confirm Friendship request', type: :feature do
     end
     user = User.find_by(email: 'carlos@gmail.com')
     friend = User.find_by(email: 'carlos2@gmail.com')
-    friends1 = Friendship.where('user_id = ? and friend_id= ?', user.id, friend.id)
+    friends1 = Friendship.where('user_id = ? and friend_id = ?', user.id, friend.id)
     expect(friends1[0].confirmed).to be(true)
+    friends_inverse = Friendship.where('user_id = ? and friend_id = ?', friend.id, user.id)
+    expect(friends_inverse[0].confirmed).to be(true)
   end
 end
-# rubocop:enable Metrics/BlockLength
