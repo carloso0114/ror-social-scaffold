afd4c51b3c54a738c25c41578fbaa1b3edcaba5a
Carlos Ospina <carlossospinag@gmail.com>
2021-04-19 12:35:02 -0500

    add method to show own post, and friend's.

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -20,12 +20,7 @@ class PostsController < ApplicationController
   private
 
   def timeline_posts
-    @timeline_posts = Post.all.where(user_id: current_user.id)
-    @all_friends = Friendship.where(user_id: current_user.id, confirmed: true)
-
-    @all_friends.each do |friend|
-      @timeline_posts += Post.where(user_id: friend.friend_id)
-    end
+    @timeline_posts ||= current_user.friends_and_own_posts.ordered_by_most_recent.includes(:user)
   end
 
   def post_params
